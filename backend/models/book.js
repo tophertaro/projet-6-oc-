a5107d652e459535c471f9ebda5bfd0d017b50d8
Phorimavong Christopher <phorimavong.c@gmail.com>
2024-10-22 17:02:40 +0200

    models done

---
+++ backend/models/book.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+const ratingSchema = mongoose.Schema({ // modèle pour les notes des utilisateurs 
+  userId: {
+    type: String,
+    required: true
+  },
+  grade: {
+    type: Number,
+    required: true
+  }
+});
+
+const bookSchema = mongoose.Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  year: {
+    type: Number,
+    required: true
+  },
+  genre: {
+    type: String,
+    required: true
+  },
+  ratings: [ratingSchema],
+  averageRating: {
+    type: Number,
+    default: 0
+  }
+})
+
+const Book = mongoose.model('Book', bookSchema)
+
+bookSchema.method.calculateAverageRating = function() {
+  // method pour calculer l'average rating
+}
+
+module.exports = Book;
