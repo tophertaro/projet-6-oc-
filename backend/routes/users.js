3f66e77e2bc607fca04bacf34b86ee53a9949bd9
Phorimavong Christopher <phorimavong.c@gmail.com>
2024-10-22 18:34:08 +0200

    authentification ok

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,10 @@
 const express = require ('express');
 const router = express.Router();
+const userCtrl = require('../controllers/users');
 
-const usersCtrl = require('../controllers/users')
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
 
-router.post('/signup', usersCtrl.signup)
-router.post('/login', usersCtrl.login)
 
 
 module.exports = router;
